0f68a38430391b8c8975811be8b125306710d2fd
Karen Etheridge <ether@cpan.org>
2014-10-07 21:55:50 -0700

    leave off the version if it is 0

--- lib/Dist/Zilla/Plugin/MakeMaker/Awesome.pm
+++ lib/Dist/Zilla/Plugin/MakeMaker/Awesome.pm
@@ -51,7 +51,7 @@ use strict;
 use warnings;
 
 {{ $perl_prereq ? qq[use $perl_prereq;] : ''; }}
-use ExtUtils::MakeMaker{{ defined $eumm_version ? ' ' . $eumm_version : '' }};
+use ExtUtils::MakeMaker{{ $eumm_version ? " $eumm_version" : '' }};
 
 {{ $header }}
 
