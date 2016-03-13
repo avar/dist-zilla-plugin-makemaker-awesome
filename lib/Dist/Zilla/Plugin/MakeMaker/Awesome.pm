2d6f4c8f5d83c71ee1862b813a0964ce37cf916c
Karen Etheridge <ether@cpan.org>
2016-03-12 16:05:35 -0800

    increment $VERSION after 0.36 release

--- lib/Dist/Zilla/Plugin/MakeMaker/Awesome.pm
+++ lib/Dist/Zilla/Plugin/MakeMaker/Awesome.pm
@@ -2,7 +2,7 @@ package Dist::Zilla::Plugin::MakeMaker::Awesome;
 # ABSTRACT: A more awesome MakeMaker plugin for L<Dist::Zilla>
 # KEYWORDS: plugin installer MakeMaker Makefile.PL toolchain customize override
 
-our $VERSION = '0.36';
+our $VERSION = '0.37';
 
 use Moose;
 use MooseX::Types::Moose qw< Str ArrayRef HashRef >;
