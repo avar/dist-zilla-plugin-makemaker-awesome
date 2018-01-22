edf05dce348988050a1b7fed2f95957843d68938
Karen Etheridge <ether@cpan.org>
2018-01-21 20:04:48 -0800

    TestRunner already adds default_jobs to metadata

--- lib/Dist/Zilla/Plugin/MakeMaker/Awesome.pm
+++ lib/Dist/Zilla/Plugin/MakeMaker/Awesome.pm
@@ -119,7 +119,6 @@ around dump_config => sub
 
     $config->{'Dist::Zilla::Plugin::MakeMaker'}{make_path} ||= $self->make_path;
     $config->{'Dist::Zilla::Plugin::MakeMaker'}{version} ||= Dist::Zilla::Plugin::MakeMaker->VERSION;
-    $config->{'Dist::Zilla::Role::TestRunner'}{default_jobs} ||= $self->default_jobs;
     $config->{'Dist::Zilla::Role::TestRunner'}{version} ||= Dist::Zilla::Role::TestRunner->VERSION;
 
     return $config;
